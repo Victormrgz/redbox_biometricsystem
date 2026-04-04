b04fbd5b0d35c5bc8121d5dea9b858fc035918c7
BriaYan1 <briayang1997@gmail.com>
2026-04-04 09:57:01 -0400

    menu de navegacion v-0.2 ( se agregaron iconos a la barra de navegacion y se agrego estilos a la misma )

--- src/navegacion/AppNavegar.jsx
+++ src/navegacion/AppNavegar.jsx
@@ -1,23 +1,65 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 /**Importar las vistas de la app*/
 
 import Inicio from "../usuario/Inicio";
-import Perfil from "../usuario/Perfil";
+import Clases from "../training/Clases";
 import MisReservas from "../usuario/MisReservas";
-import CrearPlanificacion from "../training/CrearPlanificacion";
+import MisResultados from "../usuario/MisResultados";
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
     return (
-            <Tab.Navigator>
-                <Tab.Screen name="Inicio" component={Inicio} />
-                <Tab.Screen name="Perfil" component={Perfil} />
-                <Tab.Screen name="Mis Reservas" component={MisReservas} />
-                <Tab.Screen name="Crear Planificacion" component={CrearPlanificacion} />
+            <Tab.Navigator
+            initialRouteName="Inicio" /**Al abrir la app es la pantalla que se muestra por defecto */
+            screenOptions={{
+                tabBarActiveTintColor: 'white', /**Color del icono activo */
+                tabBarInactiveTintColor: '#757575', /**Color del icono inactivo */
+                tabBarStyle: { backgroundColor: '#262626' }, /**Color de fondo de la barra de navegación */
+            }}
+            >
+                <Tab.Screen 
+                    name="Inicio" 
+                    component={Inicio} 
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome name="home" size={24} color={color} />
+                        ),
+                    }}
+
+                />
+                <Tab.Screen 
+                    name="Clases" 
+                    component={Clases} 
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="edit-note" size={24} color={color} />
+                        ),
+                    }}
+                />
+                <Tab.Screen 
+                    name="Mis Reservas" 
+                    component={MisReservas} 
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="event-note" size={24} color={color}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen 
+                    name="Mis Resultados" 
+                    component={MisResultados} 
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="launch" size={24} color={color} />
+                        ),
+                    }}
+                />
             </Tab.Navigator>
     );
 }
